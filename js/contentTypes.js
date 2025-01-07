fe3d4012748e98387edb086a703866550b5587d0
terenceboakye <terenceboakye27@gmail.com>
2025-01-07 06:23:08 +0000

    desc : Maps filetypes to their headers values for usage during responses

---
+++ js/contentTypes.js
@@ -0,0 +1,28 @@
+//@desc :   mapping filetypes to their headers values for usage during responses
+
+modules.exports = {
+//html files
+    html: {
+        "Content-Type": "text/html"
+    },
+//text files
+    text :{
+        "Content-Type": "text/plain"
+    },
+//css files
+    css: {
+        "Content-Type": "text/css"
+    },
+//js files
+    js: {
+        "Content-Type": "text/javascript"
+    },
+//image(png)
+    png: {
+        "Content-Type": "image/png"
+    },    
+//image(jpg)
+    jpg: {
+        "Content-Type": "image/jpg"
+    }   
+}
