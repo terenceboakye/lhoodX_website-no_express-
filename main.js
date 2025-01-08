98c618c789a96aeafb590486b75b1c6cf01c038f
terenceboakye <terenceboakye27@gmail.com>
2025-01-08 22:42:09 +0000

    @updated: Registering individual routes with the router module

--- main.js
+++ main.js
@@ -25,6 +25,48 @@ const port = 3000,
     contentsType = require("./contentType");
     utils = require("./utils");
 
+
+//Registering individual routes with the router module\
+
+//--for Homepage
+router.get("/", (req, res) => {
+    res.writeHead(200, contentsType.html);
+    utils.getFile("views/home.html", res);
+});
+
+//--for course
+router.get("/menu", (req, res) => {
+    res.writeHead(200, contentsType.html);
+    utils.getFile("views/courses.html", res);
+});
+
+//---for About
+router.get("/about", (req, res) => {
+    res.writeHead(200, contentsType.html);
+    utils.getFile("views/about.html", res);
+});
+
+//---for Contact
+router.get("/contact", (req, res) => {
+    res.writeHead(200, contentsType.html);
+    utils.getFile("views/contact.html", res);
+});
+
+//--for Thanks after form submission
+router.post("/", (req, res) => {
+    res.writeHead(200, contentsType.html);
+    utils.getFile("views/thanks.html", res);
+});
+
+//--css
+router.get("/styles.css", (req, res) => {
+    res.writeHead(200, contentsType.css);
+    utils.getFile("views/css/styles.css", res);
+});
+
+//Starting the server...
+http.createServer(router.handle).listen(port);
+
 console.log(`The server has started and is listening on port number: ${port}`);
 
 
